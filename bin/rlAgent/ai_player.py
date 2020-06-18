fa83bc9c0b925c0a763794f3303da5f502203ea5
Jana-Z <github3@jana-zeller.de>
2020-06-18 21:36:55 +0200

    Fixed save_model

--- bin/rlAgent/ai_player.py
+++ bin/rlAgent/ai_player.py
@@ -82,7 +82,7 @@ class AI_player():
 
   def save_model(self):
     # serialize model to JSON
-    model_json = model.to_json()
+    model_json = self.model.to_json()
     with open("model.json", "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
